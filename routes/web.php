9b95f89a29f8277b4aa0f0498f760951e5355865
Demilade Soremekun <demiladesoremekun@gmail.com>
2019-11-26 17:32:32 +0100

    Completion of Task List & Details under Analytics

--- routes/web.php
+++ routes/web.php
@@ -215,16 +215,16 @@ Route::bind('downtimes', function($value, $route) {
 
 Route::match(['get', 'post'], 'analytics/ratings', [
     'as' => 'analytics.ratings', 'uses' => 'AnalyticsController@ratings'
-]);
+])->middleware(['auth.user', 'auth.access:'.$link_id.',Supervisor']);
 Route::match(['get', 'post'], 'analytics/status', [
     'as' => 'analytics.status', 'uses' => 'AnalyticsController@status'
-]);
+])->middleware(['auth.user', 'auth.access:'.$link_id.',Supervisor']);
 Route::match(['get', 'post'], 'analytics/directory', [
     'as' => 'analytics.directory', 'uses' => 'AnalyticsController@directory'
-]);
+])->middleware(['auth.user', 'auth.access:'.$link_id.',Supervisor']);
 Route::get('analytics/{request}/details', [
     'as' => 'analytics.details', 'uses' => 'AnalyticsController@details'
-]);
+])->middleware(['auth.user', 'auth.access:'.$link_id.',Supervisor']);
 
 Route::get('client', [
     'as' => 'client.index', 'uses' => 'ClientController@index'
